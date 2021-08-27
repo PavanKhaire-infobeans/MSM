bc6b953617ae53ee1aed68ae4e4083469a5bc0c4
Nikita <nikitan.jain@infobeans.com>
2021-08-27 16:48:11 +0530

    code formatting with prettier for eventManager.js

--- src/common/eventManager.ts
+++ src/common/eventManager.ts
@@ -1,27 +1,30 @@
 type Props = {
-    id: string,
-    onComplete: Function
-}
+  id: string;
+  onComplete: Function;
+};
 export default class EventManager {
-    static eventListners: {[id: string]: Function} = {}
-    id?: string;
-    constructor(props: Props) {
-        EventManager.eventListners = {...EventManager.eventListners, [props.id]: props.onComplete};
-        this.id = props.id;
-    }
+  static eventListners: {[id: string]: Function} = {};
+  id?: string;
+  constructor(props: Props) {
+    EventManager.eventListners = {
+      ...EventManager.eventListners,
+      [props.id]: props.onComplete,
+    };
+    this.id = props.id;
+  }
 
-    removeListener = () => {
-        delete EventManager.eventListners[this.id]
-    }
+  removeListener = () => {
+    delete EventManager.eventListners[this.id];
+  };
 
-    static addListener(identifier: string, onComplete: Function) {
-        return new EventManager({id: identifier, onComplete})
-    }
+  static addListener(identifier: string, onComplete: Function) {
+    return new EventManager({id: identifier, onComplete});
+  }
 
-    static callBack(identifier: string, ...args: any[]) {
-        let onComplete = EventManager.eventListners[identifier]
-        if (typeof onComplete == "function") {
-            onComplete(...args);
-        }
+  static callBack(identifier: string, ...args: any[]) {
+    let onComplete = EventManager.eventListners[identifier];
+    if (typeof onComplete == 'function') {
+      onComplete(...args);
     }
+  }
 }
