9cc00f12be62c5b0efe1193b0353402aa1464738
Nikita <nikitan.jain@infobeans.com>
2021-08-27 16:44:33 +0530

    Code formating for Image's index.js file using pretier

--- src/images/index.ts
+++ src/images/index.ts
@@ -32,82 +32,82 @@ export const chevron_down = require('./mindPop/chevron_down.png');
 export const searchIcon = require('./common/search.png');
 export const searchCancel = require('./cross/cross_icon.png');
 
-export const rubbish = require('./rubbish/rubbish-bin.png')
-export const record = require('./record-audio/recording.png')
-export const camera = require('./select-image/photo-camera.png')
+export const rubbish = require('./rubbish/rubbish-bin.png');
+export const record = require('./record-audio/recording.png');
+export const camera = require('./select-image/photo-camera.png');
 
-export const action_camera = require('./action_camera/photo-camera.png')
-export const action_audio = require('./action_audio/action_audio.png')
-export const action_pdf = require('./action_pdf/action_pdf.png')
-export const action_close = require('./action_close/close.png')
-export const action_picture = require('./action_picture/picture.png')
+export const action_camera = require('./action_camera/photo-camera.png');
+export const action_audio = require('./action_audio/action_audio.png');
+export const action_pdf = require('./action_pdf/action_pdf.png');
+export const action_close = require('./action_close/close.png');
+export const action_picture = require('./action_picture/picture.png');
 
 export const backBtn = require('./back/back.png');
 export const backBlkBtn = require('./back-black/back-black.png');
 export const recordStart = require('./recordStart/recordStart.png');
 
 export const cell_selected = require('./cell_selection/cell_selected.png');
-export const cell_unselected = require('./cell_selection/cell_unselected.png')
-export const audio_play = require('./audio_kit/play.png')
-export const audio_play_ = require('./audio_kit/play_.png')
-export const audio_stop = require('./audio_kit/stop.png')
-export const audio_pause = require('./audio_kit/pause.png')
-export const audio_pause_ = require('./audio_kit/pause_.png')
+export const cell_unselected = require('./cell_selection/cell_unselected.png');
+export const audio_play = require('./audio_kit/play.png');
+export const audio_play_ = require('./audio_kit/play_.png');
+export const audio_stop = require('./audio_kit/stop.png');
+export const audio_pause = require('./audio_kit/pause.png');
+export const audio_pause_ = require('./audio_kit/pause_.png');
 export const audio_details_open = require('./audio_kit/open_details.png');
 export const audio_details_close = require('./audio_kit/close_details.png');
 export const music_note = require('./audio_kit/music_note.png');
 
-export const sound_wave = require('./sound_wave/sound_wave.png')
-export const mainIcon = require('./mainIcon/mainIconLarge.png')
-export const splashText = require('./splashText/splashTextLarge.png')
-export const icon = require('./icon/icon.png')
-export const thumb_text_mindpop = require('./mindPop/thumb_text_mindpop.png')
-export const thumb_audio_mindpop = require('./mindPop/thumb_audio_mindpop.png')
+export const sound_wave = require('./sound_wave/sound_wave.png');
+export const mainIcon = require('./mainIcon/mainIconLarge.png');
+export const splashText = require('./splashText/splashTextLarge.png');
+export const icon = require('./icon/icon.png');
+export const thumb_text_mindpop = require('./mindPop/thumb_text_mindpop.png');
+export const thumb_audio_mindpop = require('./mindPop/thumb_audio_mindpop.png');
 
-export const visibility = require('./visibility/visibility.png')
-export const visibility_off = require('./visibility/visibility-off.png')
+export const visibility = require('./visibility/visibility.png');
+export const visibility_off = require('./visibility/visibility-off.png');
 
-export const checkbox_active= require('./cell_selection/cell_selected.png');
-export const checkbox = require('./cell_selection/cell_unselected.png')
-export const radio_active= require('./cell_selection/radio_selected.png');
-export const radio = require('./cell_selection/radio_unselected.png')
+export const checkbox_active = require('./cell_selection/cell_selected.png');
+export const checkbox = require('./cell_selection/cell_unselected.png');
+export const radio_active = require('./cell_selection/radio_selected.png');
+export const radio = require('./cell_selection/radio_unselected.png');
 
-export const edit_icon = require('./profile/icon_edit.png')
-export const icon_location = require('./profile/icon_location.png')
-export const icon_mail = require('./profile/icon_mail.png')
-export const icon_telephone = require('./profile/icon_telephone.png')
-export const profile_placeholder = require('./profile/default_profile_icon.png')
-export const default_cover_img = require('./profile/default_cover_image.png')
+export const edit_icon = require('./profile/icon_edit.png');
+export const icon_location = require('./profile/icon_location.png');
+export const icon_mail = require('./profile/icon_mail.png');
+export const icon_telephone = require('./profile/icon_telephone.png');
+export const profile_placeholder = require('./profile/default_profile_icon.png');
+export const default_cover_img = require('./profile/default_cover_image.png');
 
-export const warning_icon = require('./warning/warning_icon.png')
-export const close_white = require('./action_close/close_white.png')
-export const small_close_white = require('./action_close/small_cross_white.png')
-export const small_close_white_ = require('./action_close/small_cross_white_.png')
-export const close_white_ = require('./action_close/close_white_.png')
-export const icon_idea = require('./myAccount/idea/idea.png')
+export const warning_icon = require('./warning/warning_icon.png');
+export const close_white = require('./action_close/close_white.png');
+export const small_close_white = require('./action_close/small_cross_white.png');
+export const small_close_white_ = require('./action_close/small_cross_white_.png');
+export const close_white_ = require('./action_close/close_white_.png');
+export const icon_idea = require('./myAccount/idea/idea.png');
 
-export const icon_logout = require('./myAccount/logout/logout.png')
-export const icon_drafts = require('./myAccount/myDrafts/myDrafts.png')
-export const icon_memories = require('./myAccount/myMemories/myMemoryCollections.png')
-export const icon_messages = require('./myAccount/myMessages/myMessages.png')
-export const icon_password = require('./myAccount/myPassword/myPassword.png')
-export const icon_tickets = require('./myAccount/myTickets/myTickets.png')
-export const icon_arrow = require('./arrow/arrow.png')
+export const icon_logout = require('./myAccount/logout/logout.png');
+export const icon_drafts = require('./myAccount/myDrafts/myDrafts.png');
+export const icon_memories = require('./myAccount/myMemories/myMemoryCollections.png');
+export const icon_messages = require('./myAccount/myMessages/myMessages.png');
+export const icon_password = require('./myAccount/myPassword/myPassword.png');
+export const icon_tickets = require('./myAccount/myTickets/myTickets.png');
+export const icon_arrow = require('./arrow/arrow.png');
 
-export const icon_people = require('./moreOptions/people/People.png')
-export const icon_events = require('./moreOptions/events/Events.png')
-export const icon_settings = require('./moreOptions/settings/settings.png')
-export const icon_faq = require('./moreOptions/faq/faq.png')
-export const icon_info = require('./moreOptions/info/info.png')
-export const icon_headset = require('./moreOptions/headset/headset.png')
+export const icon_people = require('./moreOptions/people/People.png');
+export const icon_events = require('./moreOptions/events/Events.png');
+export const icon_settings = require('./moreOptions/settings/settings.png');
+export const icon_faq = require('./moreOptions/faq/faq.png');
+export const icon_info = require('./moreOptions/info/info.png');
+export const icon_headset = require('./moreOptions/headset/headset.png');
 
-export const icon_add_content_audio = require('./add_content/audio/recording.png')
-export const icon_add_content_camera = require('./add_content/camera/camera.png')
-export const icon_add_content_write = require('./add_content/write/write.png')
-export const icon_add_content_upload = require('./add_content/upload/upload.png')
+export const icon_add_content_audio = require('./add_content/audio/recording.png');
+export const icon_add_content_camera = require('./add_content/camera/camera.png');
+export const icon_add_content_write = require('./add_content/write/write.png');
+export const icon_add_content_upload = require('./add_content/upload/upload.png');
 
-export const icon_upload_file = require('./action_upload/upload_files.png')
-export const icon_collaborators = require('./memory/collaborators/collaborators.png')
+export const icon_upload_file = require('./action_upload/upload_files.png');
+export const icon_collaborators = require('./memory/collaborators/collaborators.png');
 
 export const delete_icon = require('./mindPop/deleteIcon.png');
 export const icon_close_black = require('./action_close/close_black.png');
@@ -131,25 +131,25 @@ export const white_arrow = require('./arrow/white_arrow.png');
 
 export const delete_comment = require('./delete_comment/delete_comment.png');
 
-export const memory_draft = require('./memory/memory_draft/memory_draft.png');
-export const memory_read = require('./memory/memory_read/memory_read.png');
-export const memory_read_all = require('./memory/memory_read/memory_read_all.png');
-export const memory_unread = require('./memory/memory_unread/memory_unread.png');
+export const memory_draft = require('./memory/memory_draft/memory_draft.png');
+export const memory_read = require('./memory/memory_read/memory_read.png');
+export const memory_read_all = require('./memory/memory_read/memory_read_all.png');
+export const memory_unread = require('./memory/memory_unread/memory_unread.png');
 export const save_memory_draft = require('./memory/memory_draft/save.png');
 export const publish_memory_draft = require('./memory/memory_draft/publish.png');
 export const add_icon_small = require('./memory/memory_draft/add_small_icon.png');
 
-export const collaborative = require("./collaborative/collaborative.png");
+export const collaborative = require('./collaborative/collaborative.png');
 
-export const downImage = require('./downImage/downImage.png')
+export const downImage = require('./downImage/downImage.png');
 
-export const upImage = require('./upImage/upImage.png')
+export const upImage = require('./upImage/upImage.png');
 
-export const itemSelectedCheckMark = require('./itemSelectedCheckMark/itemSelectedCheckMark.png')
+export const itemSelectedCheckMark = require('./itemSelectedCheckMark/itemSelectedCheckMark.png');
 
-export const move_arrow = require('./memory_collections/move-arrows.png');
-export const settings_icon = require('./memory_collections/settings.png');
-export const visibility_theme = require('./memory_collections/visibility.png');
+export const move_arrow = require('./memory_collections/move-arrows.png');
+export const settings_icon = require('./memory_collections/settings.png');
+export const visibility_theme = require('./memory_collections/visibility.png');
 export const add_icon = require('./memory_collections/add_icon.png');
 export const team_icon = require('./team/team.png');
 
@@ -159,7 +159,7 @@ export const greenDotsButton = require('./greenDotsButton/greenDotsButton.png');
 
 export const delete_memory = require('./delete_memory/delete_memory.png');
 export const edit_memory = require('./edit_memory/edit_memory.png');
-export const move_to_draft = require ('./move_to_draft/move_to_draft.png');
+export const move_to_draft = require('./move_to_draft/move_to_draft.png');
 export const remove_me_from_this_post = require('./remove_me_from_this_post/remove_me_from_this_post.png');
 export const block_memory = require('./block_memory/block_memory.png');
 export const block_user = require('./block_user/block_user.png');
@@ -193,18 +193,17 @@ export const registrationGreenLight = require('./registration/registration_green
 export const registrationOrangeDark = require('./registration/registration_orange_dark.png');
 export const registrationOrangeLight = require('./registration/registration_orange_light.png');
 export const background_msm = require('./registration/background_msm.png');
-export const recordRegistration = require('./registration/record_registration.png')
+export const recordRegistration = require('./registration/record_registration.png');
 
-export const search_illustration = require('./common/search_illustration.png')
-export const backArrowWhite = require('./arrow/white_arrow_back.png')
-export const registration_vector = require('./common/registration_vector.png')
-
+export const search_illustration = require('./common/search_illustration.png');
+export const backArrowWhite = require('./arrow/white_arrow_back.png');
+export const registration_vector = require('./common/registration_vector.png');
 
 export const search_theme = require('./common/search_icon.png');
 export const login_icon = require('./common/login_icon.png');
 export const user_drawable = require('./common/email_drawable.png');
-export const info_icon = require('./common/info_icon.png')
-export const info_icon_ = require('./common/info_icon_.png')
+export const info_icon = require('./common/info_icon.png');
+export const info_icon_ = require('./common/info_icon_.png');
 export const google_icon = require('./google_signin/google_icon.png');
 export const apple_icon = require('./apple_signin/apple_signin.png');
 
@@ -214,8 +213,8 @@ export const blue_head_icon = require('./branding_assets/blue_head_icon.png');
 export const white_head_icon = require('./branding_assets/white_head_icon.png');
 export const msm_coloured_banner = require('./branding_assets/msm_coloured_banner.png');
 
-export const filter_icon = require('./filters/filter_icon.png')
-export const calendar = require('./calendar/calendar.png')
+export const filter_icon = require('./filters/filter_icon.png');
+export const calendar = require('./calendar/calendar.png');
 
 export const guidedTourHelper = require('./guided_tour_helper/guidedTourHelper.png');
 export const close_guide_tour = require('./close_guide_tour/close_guide_tour.png');
@@ -240,11 +239,11 @@ export const close_big = require('./close_big/close_big.png');
 export const msm_preserveYourMemories = require('./msm_preserveYourMemories/msm_preserveYourMemories.png');
 export const msm_logo = require('./msm_logo/msm_logo.png');
 export const close_big_grey = require('./close_big_grey/close_big_grey.png');
-export const exit_tour =  require('./exit_tour/exit_tour.png');
-export const arrow_left =  require('./arrow_left/arrow_left.png');
-export const arrow_right =  require('./arrow_right/arrow_right.png');
-export const progress_dot =  require('./progress_dot/progress_dot.png');
-export const progress_dot_check =  require('./progress_dot_check/progress_dot_check.png');
+export const exit_tour = require('./exit_tour/exit_tour.png');
+export const arrow_left = require('./arrow_left/arrow_left.png');
+export const arrow_right = require('./arrow_right/arrow_right.png');
+export const progress_dot = require('./progress_dot/progress_dot.png');
+export const progress_dot_check = require('./progress_dot_check/progress_dot_check.png');
 export const msm_allPages_mindPop = require('./msm_allPages_mindPop/msm_allPages_mindPop.png');
 export const check = require('./check/check.png');
 export const plus = require('./plus/plus.png');
